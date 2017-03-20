03ff56a4de61141c6ba0e2bba010ee5aeb45e20d
kagentry <keytheadam@gmail.com>
2017-03-19 21:03:33 -0400

    js additons before creating testing branch for js code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,30 @@
+/////////GLOBAL VARIABLES
+var artist = "";
+
+////////FUNCTIONS
+
+// grabs user input and populates APIs
+function initiateSearch(name){
+
+}
+
 $(document).ready(function(){
-      $('.carousel').carousel();
-      // Next slide
-$('.carousel').carousel('next');
-$('.carousel').carousel('next', 3); // Move next n times.
-// Previous slide
-$('.carousel').carousel('prev');
-$('.carousel').carousel('prev', 4); // Move prev n times.
-// Set to nth slide
-$('.carousel').carousel('set', 4);
-      
-    });
-        
+
+	// get the artist name from input field
+	$("#search").keypress(function(event){
+		if (event.which == 13){
+			artist = $("#search").val().trim();
+		}
+	});
+	console.log("here: " + artist);
+
+	$('.carousel').carousel();
+	// Next slide
+	$('.carousel').carousel('next');
+	$('.carousel').carousel('next', 3); // Move next n times.
+	// Previous slide
+	$('.carousel').carousel('prev');
+	$('.carousel').carousel('prev', 4); // Move prev n times.
+	// Set to nth slide
+	$('.carousel').carousel('set', 4);
+});
